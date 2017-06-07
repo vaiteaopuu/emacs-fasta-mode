d928c2757fc56cad5ebb0d0b526e3ce1ab18e91d
vaiteaopuu <vaiteaopuu@gmail.com>
2017-06-07 08:57:42 +0200

    data file

---
+++ python-src/data.py
@@ -0,0 +1,14 @@
+RESIDUE_3 = [
+    "ALA", "CYS", "GLU", "ASP", "PHE", "ILE", "HIS", "LYS", "MET", "LEU",
+    "ASN", "GLN", "SER", "ARG", "THR", "TRP", "VAL", "TYR", "PRO", "GLY"
+]
+
+RESIDUE_1 = [
+    "A", "C", "E", "D", "F", "I", "H", "K", "M", "L", "N", "Q", "S", "R", "T",
+    "W", "V", "Y", "P", "G"
+]
+
+RESIDUE_GROUP = [
+    "ACT", "ACT", "ED", "ED", "FW", "IVL", "H", "K", "M", "IVL", "NQ", "NQ",
+    "S", "R", "ACT", "FW", "IVL", "Y", "PG", "PG"
+]
